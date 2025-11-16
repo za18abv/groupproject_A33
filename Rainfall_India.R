f5484b9a30a79b88ecdd4f2774b6827ea2eb973e
Mershack-d <md25aas@herts.ac.uk>
2025-11-16 18:13:54 +0000

    added code to generate decades for each sub division using years

--- Rainfall_India.R
+++ Rainfall_India.R
@@ -1,5 +1,8 @@
 print(colnames(rainfall_india))
 
+#Creating Decades using the years
+rainfall_india$Decade <- paste0((rainfall_india$YEAR %/% 10) * 10, "s")
+
 # printing first 5 rows
 print(head(rainfall_india, 5))
 
