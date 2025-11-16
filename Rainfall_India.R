c9dca1ee3b4be6143c6cdd7a90489c48a5a95f5d
pt25aao <pt25aao@herts.ac.uk>
2025-11-16 20:53:58 +0000

    Added code to import dataset in R script

--- Rainfall_India.R
+++ Rainfall_India.R
@@ -1,8 +1,12 @@
-print(colnames(rainfall_india))
+# Import the Dataset
+library(readr)
+rainfall_india <- read_csv("rainfall_india.csv")
+View(rainfall_india)
 
-#Creating Decades using the years
-rainfall_india$Decade <- paste0((rainfall_india$YEAR %/% 10) * 10, "s")
+print(colnames(rainfall_india))
 
 # printing first 5 rows
 print(head(rainfall_india, 5))
 
+#Creating Decades using the years
+rainfall_india$Decade <- paste0((rainfall_india$YEAR %/% 10) * 10, "s")
