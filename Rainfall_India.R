10cf3455c71ff89e262d22633a81e4692c639bbd
Mershack-d <md25aas@herts.ac.uk>
2025-11-17 05:52:47 +0000

    refactor: refactored to take out 's' in decades to keep datatype as integer

--- Rainfall_India.R
+++ Rainfall_India.R
@@ -8,8 +8,9 @@ print(colnames(rainfall_india))
 # printing first 5 rows
 print(head(rainfall_india, 5))
 
-#Creating Decades using the years
-rainfall_india$Decade <- paste0((rainfall_india$YEAR %/% 10) * 10, "s")
-
 # Creates a dataset without NA values
 rf_ds <- subset(rainfall_india, rainfall_india$ANNUAL > 60)
+
+#Creating Decades using the years
+rainfall_india$Decade <- (rainfall_india$YEAR %/% 10) * 10
+
