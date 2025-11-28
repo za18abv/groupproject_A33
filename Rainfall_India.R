2066812c45fc68d5d71973a0f5d9c3285c2e920e
ho24aas <ho24aas@herts.ac.uk>
2025-11-28 14:22:17 +0000

    Calculate mean annual rainfall per region per decade. Find decade with the highest mean annual rainfall. Return decade.

--- Rainfall_India.R
+++ Rainfall_India.R
@@ -24,8 +24,22 @@ library(tidyverse)
 #Rename column from SUBDIVISION to REGION
 colnames (rainfall_india)[1] <- "REGION"
 
-#Calculate mean annual rainfall for the different regions
-mean_annual_rainfall_per_region <- rainfall_india %>% group_by(REGION) %>% summarize( MeanAnnualRainfall = mean(ANNUAL, na.rm = TRUE) )
+#Calculate mean annual rainfall per region per decade
+mean_annual_rainfall_per_region_per_decade <- rainfall_india %>% group_by(REGION, Decade) %>% summarize( MeanAnnualRainfall = mean(ANNUAL, na.rm = TRUE), .groups = "drop" )
 
-#View mean_annual_rainfall_per_region data table created
-View(mean_annual_rainfall_per_region)
+#View mean annual rainfall per region per decade table created
+View(mean_annual_rainfall_per_region_per_decade)
+
+#Sort table in descending order of mean annual rainfall
+sorted_meanAnnualRainfall_table <- mean_annual_rainfall_per_region_per_decade %>%
+  arrange(desc(MeanAnnualRainfall))
+
+#View sorted table (to see decade with the highest mean annual rainfall)
+View(sorted_meanAnnualRainfall_table)
+
+#Return data for the 1910s decade (decade with the highest mean annual rainfall = 5090.100)
+decade_1910_table <- mean_annual_rainfall_per_region_per_decade %>%
+  filter(Decade == 1910)
+
+#View the 1910s decade table
+View(decade_1910_table)
